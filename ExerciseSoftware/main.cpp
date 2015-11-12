5be9722f5a0bc450a881e46c77cf0492d8b86aa3
jorshi <jordieshier@gmail.com>
2015-11-11 16:11:50 -0800

    Initial Commit

---
+++ ExerciseSoftware/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ExerciseSoftware
+//
+//  Created by Jordie Shier  on 2015-11-11.
+//  Copyright © 2015 Jordie Shier . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
